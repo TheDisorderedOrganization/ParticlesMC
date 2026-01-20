29edf39905fb7421b6544086e6895858cbfe81fc
François Villemot <francois.villemot@espci.fr>
2026-01-20 13:51:33 +0100

    perf: add inbounds keywords

--- src/neighbours.jl
+++ src/neighbours.jl
@@ -214,7 +214,7 @@ function (cell_list::CellList)(system::Particles, i::Int)
     neighbour_cells = cell_list.neighbour_cells[c]
     # Scan the neighbourhood of cell mc (including itself)
     # and from there scan atoms in cell c2
-    return (j for c2 in neighbour_cells for j in @inbounds cell_list.cells[c2])
+    return (j for c2 in @inbounds neighbour_cells for j in @inbounds cell_list.cells[c2])
 end
 
 
@@ -331,17 +331,17 @@ function Base.iterate(neighbour_list::LinkedIterator, state=-1)
             return nothing
         end
         c, c_state = next
-        j = neighbour_list.head[c]
+        @inbounds j = neighbour_list.head[c]
     else
         c_state, j = state
-        j = neighbour_list.list[j]
+        @inbounds j = neighbour_list.list[j]
         if j == -1
             next = iterate(neighbour_list.neighbour_cells, c_state)
             if next == nothing
                 return nothing
             end
             c, c_state = next
-            j = neighbour_list.head[c]
+            @inbounds j = neighbour_list.head[c]
         end
     end
 
