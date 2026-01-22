60dcb217ff088ff0c8e743999f012f5cf79e215b
François Villemot <francois.villemot@espci.fr>
2026-01-22 14:15:30 +0100

    fix: update parameters

--- examples/ortho-terphenyl/2-equilibrate-at-different-temperatures/create-folders.sh
+++ examples/ortho-terphenyl/2-equilibrate-at-different-temperatures/create-folders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for temperature in 3.0 2.0 1.6 1.4 1.25 1.2 1.15 1.1 1.05 1
+for temperature in 3.0 2.0 1.6 1.4 1.25 1.2 1.15 1.1 1.05 1.0
 do
     mkdir -p $temperature
     sed "s/TEMPERATURE/$temperature/" params-template.toml > $temperature/params.toml
