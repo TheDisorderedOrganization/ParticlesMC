d400601a4fa44240d51219d618a648bd60d1d36e
cyfraysse <fraysse.cyprien@gmail.com>
2026-05-12 10:44:45 +0200

    add initialise and finalise for MSADTracker

--- src/msad.jl
+++ src/msad.jl
@@ -79,9 +79,56 @@ end
 function MSADTracker(chains, theta_T::T, output_schedule::Vector{Int}, path::String) where {T}
     n = length(chains)
     states = [MSADState{T}() for _ in 1:n] # one empty state per chain filled during initialise
-    
+
     dirs  = [joinpath(path, "chains", "$c") for c in 1:n]
     paths = [joinpath(d, "msad.dat") for d in dirs]
     files = Vector{IOStream}(undef, n)
     return MSADTracker{T}(states, theta_T, output_schedule, paths, files)
+end
+
+##### Initialisation of the simulation #####
+# called once at t = 0, set the system, the mutable struct and the storing paths 
+##########                        ##########
+
+function Arianna.initialise(tracker::MSADTracker, simulation::Simulation)
+
+    for c in eachindex(simulation.chains)
+        system = simulation.chains[c]
+        state  = tracker.states[c]
+        T      = typeof(system.temperature)   # get the float type from system so one can choose Float64 or 32 (generic)
+
+        # create output directory if it doesn't exist
+        mkpath(dirname(tracker.paths[c]))
+
+        # open output file — write header
+        tracker.files[c] = open(tracker.paths[c], "w")
+        println(tracker.files[c], "# t  msad_euler  msad_integral  msad_thresh")
+
+        # compute initial body frames
+        R_all = get_all_body_frames(system)
+        N_mol = system.Nmol
+
+        # fill state, copy. so they are different pointers!
+        state.R0           = copy(R_all)
+        state.R_prev       = copy(R_all)
+        state.phi_integral = [zero(SVector{3,T}) for _ in 1:N_mol]
+        state.R_ref_thresh = copy(R_all)
+        state.phi_acc      = [zero(SVector{3,T}) for _ in 1:N_mol]
+        state.initialized  = true
+
+        # write t=0, all MSAD values are zero at start
+        println(tracker.files[c], "0 0.0 0.0 0.0")
+        flush(tracker.files[c])
+    end
+
+end
+
+##### Finalise th simulation #####
+# close output to not keep unwritten data in memory
+##########              ##########
+
+function Arianna.finalise(tracker::MSADTracker, ::Simulation)
+    for f in tracker.files
+        close(f)
+    end
 end
